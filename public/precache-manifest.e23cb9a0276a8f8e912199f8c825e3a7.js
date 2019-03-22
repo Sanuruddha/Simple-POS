72fce4d33365b2acbaaa6ba799b5e43270be03d9
Shihan <shihananuruddha@gmail.com>
2019-03-22 13:01:43 +0530

    adds client build in public folder

---
+++ public/precache-manifest.e23cb9a0276a8f8e912199f8c825e3a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "88b53f490bf557b7e5a9",
+    "url": "/static/css/main.081bd0f2.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3a4667edb0783fdcfd1b",
+    "url": "/static/js/2.3a4667ed.chunk.js"
+  },
+  {
+    "revision": "88b53f490bf557b7e5a9",
+    "url": "/static/js/main.88b53f49.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3a4667edb0783fdcfd1b",
+    "url": "/static/css/2.a32ce1cc.chunk.css"
+  },
+  {
+    "revision": "173443716125dba724a093e4cb1ffb77",
+    "url": "/index.html"
+  }
+];
