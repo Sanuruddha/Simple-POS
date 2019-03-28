0483a034fe3139e41bdc8464c4c5b56011b708b2
Shihan <shihananuruddha@gmail.com>
2019-03-28 08:19:50 +0530

    adds front end client build version 2.0

--- public/precache-manifest.dbb652286dfefc1ef603ab8458ff6968.js
+++ public/precache-manifest.dbb652286dfefc1ef603ab8458ff6968.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "7e647360a35b26fd93a2",
-    "url": "/static/css/main.7f9612f3.chunk.css"
+    "revision": "f249dacca5e7967b642f",
+    "url": "/static/css/main.ed66386b.chunk.css"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/static/js/2.6528e6f0.chunk.js"
   },
   {
-    "revision": "7e647360a35b26fd93a2",
-    "url": "/static/js/main.7e647360.chunk.js"
-  },
-  {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+    "revision": "f249dacca5e7967b642f",
+    "url": "/static/js/main.f249dacc.chunk.js"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
     "revision": "6528e6f002382a3edc5c",
     "url": "/static/css/2.62031f83.chunk.css"
   },
   {
-    "revision": "63313e630d4bb1112a46bda08e5c31f8",
+    "revision": "7d732e77baa88fb264b830b23c136308",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e26ab7bf3d64baa8018d98da89cbb968.js"
+  "/precache-manifest.dbb652286dfefc1ef603ab8458ff6968.js"
 );
 
 workbox.clientsClaim();
